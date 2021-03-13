e0eddce20d0936b3c6a6d3d09c8f8cb83ebe5ae3
Scian <65375075+hoony6134@users.noreply.github.com>
2021-03-13 16:01:20 +0900

    Create humidity.py

---
+++ humidity.py
@@ -0,0 +1,13 @@
+import time
+import board
+import adafruit_dht
+
+dhtDevice = adafruit_dht.DHT11(board.D4)
+while True:
+    try:
+        temperature_c = dhtDevice.temperature
+        humidity = dhtDevice.humidity
+        print("temp: {}C Humidity : {}%.".format(temperature_c, humidity))
+    except RuntimeError as error:
+        pass
+    time.sleep(1)
